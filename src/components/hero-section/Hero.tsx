89a4c7567e8f641b4611d7e221eb8df8bc89f101
Aboagye=Heming <hemingkwabena@gmail.com>
2024-07-07 22:28:09 +0000

    styled hero section

--- src/components/hero-section/Hero.tsx
+++ src/components/hero-section/Hero.tsx
@@ -2,23 +2,16 @@ import "./hero.css";
 
 const Hero = () => {
   return (
-    <>
-      <section className="banner-area">
-        <div className="banner-img"></div>
-        <div className="banner-text">
-          <h3>Welcome To First Baptist Church</h3>
-          {/* <h1>
-            God is <span className="text-red">Almighty</span>
-          </h1> */}
-          {/* <a
-            href="#"
-            className="banner-btn"
-          >
-            More Info
-          </a> */}
-        </div>
-      </section>
-    </>
+    <section className="banner-area">
+      <div className="banner-img img1"></div>
+      <div className="banner-img img2"></div>
+      <div className="banner-img img3"></div>
+      <div className="banner-img img4"></div>
+      <div className="banner-img img5"></div>
+      <div className="banner-text">
+        <h3>Welcome To First Baptist Church</h3>
+      </div>
+    </section>
   );
 };
 
